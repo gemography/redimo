9787d88578866d674767f44cfb1400e67fb93da8
Ahmed Lekssays <lekssaysahmed@gmail.com>
2018-07-25 15:38:03 +0100

    connect to replicaset

--- app.go
+++ app.go
@@ -1,1 +1,27 @@
-// Entry Point
+package main
+
+import(
+  "fmt"
+  "github.com/globalsign/mgo"
+  "log"
+)
+type DataStore struct {
+	session *mgo.Session
+}
+
+func (ds *DataStore) getCol(colName string) *mgo.Collection {
+	return ds.session.Copy().DB("project").C("users")
+}
+
+func main() {
+    fmt.Println("Starting...")
+    var err error
+    const url = "mongodb://41.79.79.212:27017,41.79.79.212:27018,41.79.79.212:27019/?replicaSet=rs0"
+    var ds DataStore
+  	ds.session, err = mgo.Dial(url)
+  	if err != nil {
+  		log.Fatal(err)
+  	} else {
+      fmt.Println("Database Connected :)")
+    }
+}
