c43d7a70f9dd6f7862bd9c5cda1b847e5a600c33
Ahmed Lekssays <lekssaysahmed@gmail.com>
2018-07-25 16:11:29 +0100

    connect to redis

--- app.go
+++ app.go
@@ -3,8 +3,10 @@ package main
 import(
   "fmt"
   "github.com/globalsign/mgo"
+  "github.com/go-redis/redis"
   "log"
 )
+
 type DataStore struct {
 	session *mgo.Session
 }
@@ -16,12 +18,24 @@ func (ds *DataStore) getCol(colName string) *mgo.Collection {
 func main() {
     fmt.Println("Starting...")
     var err error
+    var client *redis.Client
+    client = redis.NewClient(&redis.Options{
+      Addr:     "localhost:6379",
+      Password: "",
+      DB:       0,
+    })
+    _, err = client.Ping().Result()
+    if err != nil {
+      log.Fatal(err)
+    } else {
+      fmt.Println("Redis Connected :)")
+    }
     const url = "mongodb://41.79.79.212:27017,41.79.79.212:27018,41.79.79.212:27019/?replicaSet=rs0"
     var ds DataStore
   	ds.session, err = mgo.Dial(url)
   	if err != nil {
   		log.Fatal(err)
   	} else {
-      fmt.Println("Database Connected :)")
+      fmt.Println("MongoDB Connected :)")
     }
 }
