c8eb3951aef5c61a582408b41389ca4f1213776e
Ahmed Lekssays <lekssaysahmed@gmail.com>
2018-07-26 14:23:52 +0100

    adding change streams

--- models/user.go
+++ models/user.go
@@ -1,1 +1,11 @@
 package models
+
+import (
+	"github.com/globalsign/mgo/bson"
+)
+
+type User struct {
+	ID       bson.ObjectId `bson:"_id"  json:"id"`
+	Username string        `bson:"username" json:"username"`
+	Email    string        `bson:"email" json:"email"`
+}
